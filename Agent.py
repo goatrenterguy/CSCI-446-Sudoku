13a6cf893c481f61dd5563da416f2f8cc0b111c0
Ben Guericke <ben.guericke@gmail.com>
2021-09-15 16:59:48 -0600

    Refactored

--- Agent.py
+++ Agent.py
@@ -2,30 +2,16 @@ from Environment import Environment
 from BacktrackingSolver import BacktrackingSolver
 
 
-class Sensor:
-    @staticmethod
-    def getState(environment: Environment):
-        return environment.getBoard()
-
-
-class Actuator:
-    @staticmethod
-    def setCell(coords: tuple, environment: Environment):
-        environment.setCell(coords)
-
-
 class Agent:
+
     def __init__(self):
-        self.sensor = Sensor()
-        self.actuator = Actuator()
         self.nextMove = []
         self.logicSteps = int
-        self.archivedEnvironments = []
         self.currentEnvironment = None
 
     #   Check if the current environment is solved
-    def isSolved(self):
-        for y in self.sensor.getState(self.currentEnvironment):
+    def isSolved(self, board: list):
+        for y in board:
             for x in y:
                 if x == 0:
                     return False
@@ -33,14 +19,17 @@ class Agent:
 
     #   Method to initialize an environment, if there is a current board archive it
     def initializeEnvironment(self, difficultly, boardNumber):
-        if self.currentEnvironment is not None:
-            self.archivedEnvironments.append(self.currentEnvironment)
         self.currentEnvironment = Environment(difficultly, boardNumber)
 
+    def solveWithBacktracking(self):
+        return BacktrackingSolver(self.currentEnvironment.getBoard()).solve()
 
-class Main:
-    agent = Agent()
-    agent.initializeEnvironment("Easy", 1)
-    solver = BacktrackingSolver(Solver(agent, agent.sensor, agent.actuator))
 
-Main()
+if __name__ == "__main__":
+    print("Enter desired board difficulty [\"Easy\", \"Med\",\"Hard\",\"Evil\"]:")
+    difficulty = input()
+    print("Enter board number [1-5]:")
+    boardNumber = input()
+    a = Agent()
+    a.initializeEnvironment(difficulty, boardNumber)
+    print(a.solveWithBacktracking())
--- BacktrackingSolver.py
+++ BacktrackingSolver.py
@@ -1,43 +1,38 @@
-
-
-
 class BacktrackingSolver:
-    
-    def __init__(self, agent:Agent):
-        self.sensor = agent.Sensor
-        self.actuator = agent.Actuator
-        self.agent = agent
+
+    def __init__(self, board: list):
+        self.board = board
+        self.logicSteps = 0
 
     def solve(self):
-        self.agent.logicStep++
-        self.agent.currentEnvironment = self.Solver.sensor.getState()
+        self.logicSteps += 1
         for x in range(8):
             for y in range(8):
-                if self.agent.currentEnvironment[x][y] == 0:
-                    for testInput in range(1,9):
-                        if possible(x, y, testInput):
-                            self.agent.currentEnvironment[x][y] = testInput
-                            solve()
-                            self.agent.currentEnvironment[x][y] = 0
-                    return
+                if self.board[x][y] == 0:
+                    for testInput in range(1, 9):
+                        if self.possible(x, y, testInput):
+                            self.board[x][y] = testInput
+                            self.solve()
+                            self.board[x][y] = 0
+                    return self.board
 
-    def possible(self, x, y, n):
+    def possible(self, x, y, testInput):
         # check row
         for k in range(8):
-            if self.agent.currentEnvironment[x][k] == testInput:
-                return false
+            if self.board[x][k] == testInput:
+                return False
 
         # check column
         for k in range(8):
-            if self.agent.currentEnvironment[k][y] == testInput:
-                return false
+            if self.board[k][y] == testInput:
+                return False
 
         # check cell
         cornerX = (x // 3) * 3
         cornerY = (y // 3) * 3
         for i in range(2):
             for j in range(2):
-                if self.agent.currentEnvironment[cornerX + i][cornerY + j] == testInput:
-                    return false
+                if self.board[cornerX + i][cornerY + j] == testInput:
+                    return False
 
-        return true
+        return True
